08844a5092a6ff8ee458e6feb24d83f2a0881675
AndreyMachula <amcz2020@gmail.com>
2022-05-07 22:38:51 +0200

    adds style import

--- parcel-project-template-main/src/js/01-gallery.js
+++ parcel-project-template-main/src/js/01-gallery.js
@@ -1,3 +1,7 @@
+// Described in the documentation
+import SimpleLightbox from "simplelightbox";
+// Additional style imports
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
