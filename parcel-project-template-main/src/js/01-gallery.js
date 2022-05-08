e0d88128f541b1585f51183ce7de8624e57ff5a1
AndreyMachula <amcz2020@gmail.com>
2022-05-08 11:01:17 +0200

    Update 01-gallery.js

--- parcel-project-template-main/src/js/01-gallery.js
+++ parcel-project-template-main/src/js/01-gallery.js
@@ -1,14 +1,16 @@
 // Described in the documentation
 import SimpleLightbox from "simplelightbox";
+
 // Additional style imports
 import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Import a markup template
 import markupTemplate from '../templates/markup-template.hbs';
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(markupTemplate);
 const galleryEl = document.querySelector('.gallery');
 
 
@@ -18,9 +20,6 @@ const createGalleryMarkup = galleryItems
   .join("");
 
 galleryEl.insertAdjacentHTML("beforeend", createGalleryMarkup);
-
-
-
 
 // Library initialization after gallery elements are created and added to `div.gallery`.
 const lightboxOptions = {
