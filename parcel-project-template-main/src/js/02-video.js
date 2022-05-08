9b5304f47957fea28605b5eb46912cdaa4e3c75c
AndreyMachula <amcz2020@gmail.com>
2022-05-08 11:04:34 +0200

    Adds script saves current video playback time to local storage

--- parcel-project-template-main/src/js/02-video.js
+++ parcel-project-template-main/src/js/02-video.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
 
+const iFrameEl = document.querySelector('iframe');
+
+// Player initialization
+const player = new Vimeo.Player(iFrameEl);
+
+// Saved playback time to local storage
+const onPlay = ({ seconds }) => {
+  localStorage.setItem('videoplayer-current-time', seconds);
+    console.log(parseInt(localStorage.getItem('videoplayer-current-time')));
+};
+
+// Usage time update
+player.on('timeupdate', throttle(onPlay, 1000));
+
+// Resuming playback from a saved position
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
